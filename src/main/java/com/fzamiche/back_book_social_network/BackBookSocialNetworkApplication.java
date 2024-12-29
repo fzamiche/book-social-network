8e0e86c74f79591c40131f90cdc1b42b2fc5a20b
Fatah ZAMICHE <zamiche.fatah@gmail.com>
2024-12-29 18:55:23 +0100

    Initialisation du projet Book Social Network [Back-End]

---
+++ src/main/java/com/fzamiche/back_book_social_network/BackBookSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.fzamiche.back_book_social_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackBookSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackBookSocialNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fzamiche/back_book_social_network/BackBookSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fzamiche.back_book_social_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackBookSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
