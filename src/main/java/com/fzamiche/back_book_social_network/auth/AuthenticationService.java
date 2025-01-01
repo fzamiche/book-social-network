e5e00e468268ce1167968149ad2b193506ec5483
Fatah ZAMICHE <zamiche.fatah@gmail.com>
2025-01-01 13:42:06 +0100

    Add authentication feature with registration endpoint
    Implemented the `AuthenticationController` to handle user registration requests. Added `AuthenticationService` to process registration logic and `RegistrationRequest` class for request validation. This sets up the foundation for user authentication in the application.

---
+++ src/main/java/com/fzamiche/back_book_social_network/auth/AuthenticationService.java
@@ -0,0 +1,12 @@
+package com.fzamiche.back_book_social_network.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    public void register(RegistrationRequest request) {
+
+
+
+    }
+}
---
+++ src/main/java/com/fzamiche/back_book_social_network/auth/RegistrationRequest.java
@@ -0,0 +1,36 @@
+package com.fzamiche.back_book_social_network.auth;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+public class RegistrationRequest {
+
+    private static final String REQUIRED_FIELD_MESSAGE = "Ce champ est requis.";
+    private static final String INVALID_EMAIL_MESSAGE = "Email non valide.";
+    private static final String PASSWORD_TOO_SHORT_MESSAGE = "Mot de passe trop court, minimum 8 caractères.";
+
+    @NotBlank(message = REQUIRED_FIELD_MESSAGE)
+    private String firstname;
+
+    @NotBlank(message = REQUIRED_FIELD_MESSAGE)
+    private String lastname;
+
+    @NotBlank(message = REQUIRED_FIELD_MESSAGE)
+    @Email(message = INVALID_EMAIL_MESSAGE)
+    private String email;
+
+    @NotBlank(message = REQUIRED_FIELD_MESSAGE)
+    @Size(min = 8, message = PASSWORD_TOO_SHORT_MESSAGE)
+    private String password;
+}
